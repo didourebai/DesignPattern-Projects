2b0558575e5ff4947b01fdd0c7adebdac4511e68
Rebai Hamida <hamida.rebai@ardia.com.tn>
2017-02-08 14:27:44 +0100

    (*) Prorotype Pattern project.

---
+++ PrototypePattern/ColorPrototype.cs
@@ -0,0 +1,10 @@
+﻿namespace PrototypePattern
+{
+    /// <summary>
+    /// The 'Prototype' abstract class
+    /// </summary>
+    abstract class ColorPrototype
+    {
+        public abstract ColorPrototype Clone();
+    }
+}
---
+++ PrototypePattern/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ColorManager colormanager = new ColorManager
+            {
+                ["red"] = new Color(255, 0, 0),
+                ["green"] = new Color(0, 255, 0),
+                ["blue"] = new Color(0, 0, 255),
+                ["angry"] = new Color(255, 54, 0),
+                ["peace"] = new Color(128, 211, 128),
+                ["flame"] = new Color(211, 34, 20)
+            };
+
+            // Initialize with standard colors
+
+            // User adds personalized colors
+
+            // User clones selected colors
+            Color color1 = colormanager["red"].Clone() as Color;
+            Color color2 = colormanager["peace"].Clone() as Color;
+            Color color3 = colormanager["flame"].Clone() as Color;
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
